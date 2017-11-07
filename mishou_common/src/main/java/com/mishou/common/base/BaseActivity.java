702bdab6675e9ea1e02dad4dca7ba1cadcf81131
xingzhezuomeng <13146296779@163.com>
2017-11-07 14:08:18 +0800

    添加base相关封装

---
+++ mishou_common/src/main/java/com/mishou/common/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.mishou.common.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ${shishoufeng} on 17/11/7.
+ * email:shishoufeng1227@126.com
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+
+
+}
