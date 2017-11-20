fa566e591025c74b025a6d929e557fc7fb089c60
史守峰 <shishoufeng@mishou.com>
2017-11-20 21:34:47 +0800

    修改相关代码，添加TBS demo

---
+++ app/src/main/java/com/mishou/demo/webview/NtiveFragment.java
@@ -0,0 +1,9 @@
+package com.mishou.demo.webview;
+
+/**
+ * Created by ${shishoufeng} on 17/11/20.
+ * email:shishoufeng1227@126.com
+ */
+
+public class NtiveFragment {
+}
---
+++ app/src/main/java/com/mishou/demo/webview/TbsFragment.java
@@ -0,0 +1,34 @@
+package com.mishou.demo.webview;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.mishou.common.base.BaseSupportFragment;
+import com.mishou.common.demo.R;
+
+/**
+ * Created by ${shishoufeng} on 17/11/20.
+ * email:shishoufeng1227@126.com
+ */
+
+public class NtiveFragment extends BaseSupportFragment {
+
+
+
+
+
+    @Override
+    protected int getLayoutView() {
+        return R.layout.fragment_native_webview;
+    }
+
+    @Override
+    protected void setOnListener(View view) {
+
+    }
+
+    @Override
+    protected void onLogic(Bundle savedInstanceState) {
+
+    }
+}
